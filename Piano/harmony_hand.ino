b94efb2c42f1095ef780f759e0143ef9eb8b1bb8
PinguBerry <pinguberrycoraline@gmail.com>
2026-06-22 11:44:20 -0300

    40V test

--- Piano/harmony_hand.ino
+++ Piano/harmony_hand.ino
@@ -1,31 +1,24 @@
-//ARDUINO UNO
-
 #include <VarSpeedServo.h>
 #include <AccelStepper.h>
 
-#define STEP_PIN 6
-#define DIR_PIN 5
+#define STEP_PIN 5
+#define DIR_PIN 4
 
 /*#define STEP_PIN 3
 #define DIR_PIN 4*/
 
 //mudar para a maozinha 2
 float dif2 = 3.70;
-float dif1 = 7.15;
+float dif1 = 7.45;
 
 int angulo = 0; // declara origem do angulo como 0°
 
-int men;
-int cont = 0;
-bool chegou = false;
 
 VarSpeedServo s11; // cria o objeto servo
 VarSpeedServo s12;
 VarSpeedServo s13;
 
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
-
-const char* cifra[] = {"FA#1", "D", "A", "FA#1", "FA#1", "D", "B", "D"};
 
 void d1p(){
   s13.write(100, 200, true);
@@ -36,31 +29,30 @@ void d1b(){
 }
 
 void d2p(){
-  s12.write(90, 250, true);
-  delay(10);
+  s12.write(110, 200, true);
+  delay(50);
   s12.write(155, 250, true);
 }
 
 void d2b(){
-  s12.write(15, 250, true);
-  delay(10);
+  s12.write(45, 200, true);
+  delay(50);
   s12.write(155, 250, true);
 }
 
 void d3p(){
-  s11.write(90, 250, true);
-  delay(10);
-  s11.write(155, 250, true);
+  s11.write(50, 200, true);
+  delay(50);
+  s11.write(130, 250, true);
 }
 
 void d3b(){
-  s11.write(15, 250, true);
-  delay(10);
-  s11.write(155, 250, true);
+  s11.write(0, 200, true);
+  delay(50);
+  s11.write(130, 250, true);
 }
 
 void setup() {
-  delay(5000);
   Serial.begin(9600);
   pinMode(7, INPUT_PULLUP);
 
@@ -76,7 +68,7 @@ void setup() {
 
   //----------- HOMING --------------
 
-  stepper.setSpeed(5000);
+  stepper.setSpeed(4000);
 
   while (digitalRead(7) == HIGH) {//low é apertado
     stepper.runSpeed();
@@ -84,13 +76,8 @@ void setup() {
 
   stepper.setCurrentPosition(0);
 
-  stepper.setAcceleration(20000);//4000
+  stepper.setAcceleration(10000);//4000
 
-  s11.write(180, 70, true);
-  s12.write(180, 70, true);
-  s13.write(180, 70, true);
-
-  move(3, cifra[cont]);
 }
 
 float passos(float dist){//com base no motor 3
@@ -117,25 +104,10 @@ float distancia(const char* tecla){
 void move(int dedo, const char* tecla){
 
   if(dedo == 1){
-    stepper.moveTo(passos(distancia(tecla) - dif1));
+    stepper.runToNewPosition(passos(distancia(tecla) - dif1));
 
   } else if(dedo == 2){
-    stepper.moveTo(passos(distancia(tecla) - dif2));
-
-  } else if(dedo == 3){
-    stepper.moveTo(passos(distancia(tecla)));
-  }
-
-}
-
-void toca(int dedo, const char* tecla){
-
-  if(dedo == 1){
-    //opa
-    d1p();//pode tirar isso depois
-
-  } else if(dedo == 2){
-
+    stepper.runToNewPosition(passos(distancia(tecla) - dif2));
     if(tecla == "A" or tecla == "B" or tecla == "D" or tecla == "E"){
       d2b();
     }
@@ -144,6 +116,7 @@ void toca(int dedo, const char* tecla){
     }
 
   } else if(dedo == 3){
+    stepper.runToNewPosition(passos(distancia(tecla)));
     
     if(tecla == "A" or tecla == "B" or tecla == "D" or tecla == "E"){
       d3b();
@@ -152,97 +125,20 @@ void toca(int dedo, const char* tecla){
       d3p();
     }
   }
+
 }
 
-bool envia = 0;
-
 void loop() {
-  //s10.write(125,70, true);a
+  //s10.write(125,70, true);
   //delay(100);
   //s10.write(180, 70, true);
   //delay(100);
   //s10.write(180, 200, true);
-  /*stepper.run();
 
-  if(stepper.distanceToGo() == 0 && chegou == 0){//ele chegou
-    Serial.println("2");
-    chegou = true;
-  }*/
-
-  stepper.run();
-
-  if(stepper.distanceToGo() == 0 && chegou == 0 && envia){//ele chegou
-    Serial.println("2");
-    chegou = true;
-  }
-
-  if(Serial.available() > 0){
-    men = Serial.read();
-
-    if(men == '1'){//é pra tocar
-      toca(3, cifra[cont]);
-
-      if(cont < 7) {
-        cont++;
-        move(3, cifra[cont]);//já fica na posição
-        chegou = 0;
-      } else {
-        cont = 0;
-        move(3, cifra[cont]);//já fica na posição
-        chegou = 0;
-      }
-
-    }
-
-    if(men == '3'){//é pra tocar
-      envia = 1;
-    }
-  }
-
-  /*if(Serial.available() > 0){
-    men = Serial.read();
-
-    if(men == '1'){//é pra tocar
-      toca(3, cifra[cont]);
-
-      if(cont < 1) {
-        cont++;
-        //move(3, cifra[cont]);//já fica na posição
-      }
-
-    }
-
-
-  }*/
+  move(3, "FA#1");
+  delay(500);
   
-  /*
-
-  stepper.run();
-
-  if(stepper.distanceToGo() == 0 && chegou == 0 && envia){//ele chegou
-    Serial.println("2");
-    chegou = true;
-  }
-
-  if(Serial.available() > 0){
-    men = Serial.read();
-
-    if(men == '1'){//é pra tocar
-      toca(3, cifra[cont]);
-
-      if(cont < 7) {
-        cont++;
-        move(3, cifra[cont]);//já fica na posição
-      }
-
-    }
-
-    if(men == '3'){//é pra tocar
-      envia = 1;
-    }
-  }
-
-  */
+  
 
  /* FAZER FUNÇÃO
   move(1, "E2");
